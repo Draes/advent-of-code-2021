f153bdd9a2f4d31cbef35796f59cece78d00bb39
David Pinheiro <david.pinheiro@mindera.com>
2021-12-08 12:59:40 +0000

    Updated day 7 solution

--- day07/day07.js
+++ day07/day07.js
@@ -10,12 +10,8 @@ function readFile(filename) {
 function getMedian(values) {
     values.sort((a, b) => a - b);
     const half = Math.floor(values.length / 2);
-
-    if (values.length % 2) {
-        return values[half];
-    }
-
-    return (values[half - 1] + values[half]) / 2.0;
+    // considering input files are always even
+    return values[half];
 }
 
 function part1(input) {
